466da0db40e36d78193b583d5faadca55a2a80a6
Kirill Pavlovskiy <kirya0703@gmail.com>
2024-12-05 11:13:46 +0300

    Deleting files in new branch

--- src/main/java/com/pelmeshka/Cakes/repositories/ImageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pelmeshka.Cakes.repositories;
-
-import com.pelmeshka.Cakes.models.Image;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImageRepository extends JpaRepository<Image, Long> {
-}
--- src/main/java/com/pelmeshka/Cakes/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.pelmeshka.Cakes.repositories;
-
-import com.pelmeshka.Cakes.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
-}
