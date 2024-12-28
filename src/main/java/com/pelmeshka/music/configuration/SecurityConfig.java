77c1bd5d93c2ab06409eb4deead189e10f944e02
Kirill Pavlovskiy <kirya0703@gmail.com>
2024-12-28 13:36:56 +0300

    Add logout button. Small fixes in security.

--- src/main/java/com/pelmeshka/music/configuration/SecurityConfig.java
+++ src/main/java/com/pelmeshka/music/configuration/SecurityConfig.java
@@ -1,12 +1,8 @@
 package com.pelmeshka.music.configuration;
 
-import com.pelmeshka.music.services.CustomUserDetailsService;
 import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -17,7 +13,6 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 @AllArgsConstructor
 public class SecurityConfig {
-    private final CustomUserDetailsService userDetailsService;
 
     @Bean
     protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -38,12 +33,6 @@ public class SecurityConfig {
                 .logout((logout) -> logout.permitAll());
 
         return http.build();
-    }
-
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .userDetailsService(userDetailsService)
-                .passwordEncoder(passwordEncoder());
     }
 
     @Bean
